ae816c0709a9dfaf5bada5c8715ccbc7d76089ea
Prakhar Agarwal <42600135+PrakhsAgarwal@users.noreply.github.com>
2019-10-22 22:00:44 +0530

    Comments added

--- First program - LED Blink/ch1.ino
+++ First program - LED Blink/ch1.ino
@@ -11,9 +11,10 @@ void setup()
 }
 
 void loop()
-{
+{ //Setting the pin where led is to High for 1000ms.
   digitalWrite(ledPin,HIGH);
   delay(1000);
+  //Setting the pin where led is to Low for 1000ms.
   digitalWrite(ledPin,LOW);
   delay(1000);
 }
